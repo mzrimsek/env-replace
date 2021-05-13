b5313b70984ed08786a27816ac35b45a5ddfc7bf
Mike Zrimsek <mikezrimsek@gmail.com>
2021-05-13 00:54:36 -0400

    better error messages

--- main.go
+++ main.go
@@ -11,28 +11,32 @@ import (
 func main() {
 	if len(os.Args) == 2 {
 		pwd, err := os.Getwd()
-		checkError(err)
+		checkError(err, "Failed to get pwd location")
 
 		var envFilePath = filepath.Join(pwd, ".env")
-		var yamlFilePath = filepath.Join(pwd, os.Args[1])
+		var fileToProcessPath = filepath.Join(pwd, os.Args[1])
 
 		envMap := getEnvMap(envFilePath)
-		var processedYamlFile = getProcessedYaml(yamlFilePath, envMap)
-		fmt.Println(processedYamlFile)
+		var processedFile = getProcessedFile(fileToProcessPath, envMap)
+		fmt.Println(processedFile)
 	} else {
-		panic("Invalid parameters")
+		printError("Invalid parameters")
 	}
 }
 
-func checkError(e error) {
+func printError(msg string) {
+	fmt.Printf("Error: %s\n", msg)
+}
+
+func checkError(e error, msg string) {
 	if e != nil {
-		panic(e)
+		printError(msg)
 	}
 }
 
 func getEnvMap(envFilePath string) map[string]string {
 	envFile, err := os.Open(envFilePath)
-	checkError(err)
+	checkError(err, "No .env file found in pwd or unable to open")
 
 	envMap := make(map[string]string)
 
@@ -54,11 +58,11 @@ func getEnvMap(envFilePath string) map[string]string {
 	return envMap
 }
 
-func getProcessedYaml(yamlFilePath string, envMap map[string]string) string {
-	yamlFile, err := os.Open(yamlFilePath)
-	checkError(err)
+func getProcessedFile(fileToProcessPath string, envMap map[string]string) string {
+	fileToProcess, err := os.Open(fileToProcessPath)
+	checkError(err, "Target file not found in pwd")
 
-	scanner := bufio.NewScanner(yamlFile)
+	scanner := bufio.NewScanner(fileToProcess)
 	scanner.Split(bufio.ScanLines)
 
 	var lines []string
@@ -76,6 +80,6 @@ func getProcessedYaml(yamlFilePath string, envMap map[string]string) string {
 		lines = append(lines, line)
 	}
 
-	yamlFile.Close()
+	fileToProcess.Close()
 	return strings.Join(lines, "\n")
 }
